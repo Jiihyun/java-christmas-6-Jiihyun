71b1677a6081bbc90613ca6a218e25e8f08040a5
Jiihyun <cwlgusc@gmail.com>
2023-11-12 04:33:37 +0900

    feat(CustomerOrderInfo): 총혜택 금액 계산 구현

--- src/main/java/christmas/domain/CustomerOrderInfo.java
+++ src/main/java/christmas/domain/CustomerOrderInfo.java
@@ -2,11 +2,14 @@ package christmas.domain;
 
 import christmas.domain.discount.DiscountManager;
 
+import static christmas.domain.discount.DisCountAmountRule.PRICE_OF_CHAMPAIGE;
+
 public class CustomerOrderInfo {
     private static final int MINIMUM_TOTAL_PURCHASED_AMOUNT = 120_000;
     private final Day day;
     private final OrderItems orderItems;
     private final boolean hasChampagne = isTotalPurchasedAmountAboveMinimum();
+    private final int totalBenefitAmount = getTotalBenefitAmount();
     private final int totalDiscountAmount = getTotalDiscountAmount();
 
 
@@ -27,5 +30,13 @@ public class CustomerOrderInfo {
         );
         discountManager.discount();
         return discountManager.getTotalDiscountAmount();
+    }
+
+    public int getTotalBenefitAmount() {
+        int sum = getTotalDiscountAmount();
+        if (hasChampagne) {
+            sum += PRICE_OF_CHAMPAIGE.value;
+        }
+        return sum;
     }
 }
--- src/main/java/christmas/domain/discount/DisCountAmountRule.java
+++ src/main/java/christmas/domain/discount/DisCountAmountRule.java
@@ -4,7 +4,9 @@ public enum DisCountAmountRule {
     CHRISTMAS_MINIMUM_DISCOUNT(1000),
     WEEKDAY_DISCOUNT(2023),
     WEEKEND_DISCOUNT(2023),
-    SPECIAL_DISCOUNT(1000);
+    SPECIAL_DISCOUNT(1000),
+    PRICE_OF_CHAMPAIGE(25_000);
+
     public final int value;
 
     DisCountAmountRule(int value) {
